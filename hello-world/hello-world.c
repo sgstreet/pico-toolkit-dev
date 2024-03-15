cd2e66c042bf7e18f6d904bebc08757fba47d237
Stephen Street <stephen@redrocketcomputing.com>
2024-03-15 09:58:28 -0700

    Add pico-toolkit submodule

--- hello-world/hello-world.c
+++ hello-world/hello-world.c
@@ -13,10 +13,7 @@
 #include <hardware/gpio.h>
 #include <hardware/uart.h>
 
-#if 0
 #include <pico/iob.h>
-#endif
-
 #include <pico/unique_id.h>
 
 #define UART_ID uart0
@@ -25,7 +22,6 @@
 #define UART_TX_PIN 0
 #define UART_RX_PIN 1
 
-#if 0
 int picolibc_putc(char c, FILE *file);
 int picolibc_getc(FILE *file);
 
@@ -39,7 +35,6 @@ int picolibc_getc(FILE *file)
 {
 	return uart_getc(UART_ID);
 }
-#endif
 
 static __attribute__((constructor)) void console_init(void)
 {
